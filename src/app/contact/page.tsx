ceaa75c8bc1c99d27785583afe05aa42ea5176dc
Zorochan404 <dram8898@gmail.com>
2025-08-23 22:45:03 +0530

    added formspree

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -47,7 +47,7 @@ export default function Contact() {
         setIsSubmitting(true);
 
         try {
-            const response = await fetch('https://formspree.io/f/YOUR_FORM_ID', {
+            const response = await fetch('https://formspree.io/f/mwpqggbb', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
